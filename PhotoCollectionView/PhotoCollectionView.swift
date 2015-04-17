3b44bc0b84badfbc587de2aefb25168d70d35469
windmemory <wind.memory.cn@gmail.com>
2015-04-17 16:09:19 -0400

    Updated for Xcode 6.3 fixed bug if pass photonumber out of bound

--- PhotoCollectionView/PhotoCollectionView.swift
+++ PhotoCollectionView/PhotoCollectionView.swift
@@ -21,36 +21,36 @@ class PhotoCollevtionView: UIView, UIScrollViewDelegate {
     
     init(outerFrame: CGRect, photoArray: NSArray, currentNumber: Int = 0) {
         photosArray = photoArray
-        currentPhoto = currentNumber
+        currentPhoto = currentNumber > photoArray.count - 1 ? photoArray.count - 1 : currentNumber
         
         //initialize the photosArray, if the photo collection has more than 3 photos, then alloc with three uiimageView
         if (photosArray.count == 1){
-            imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as UIImage, frame: outerFrame)])
+            imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as! UIImage, frame: outerFrame)])
             currentViewNumber = 0
-            (imageViewArray[0] as ZoomablePhotoView).pageNumber = 0
+            (imageViewArray[0] as! ZoomablePhotoView).pageNumber = 0
         }else if (photosArray.count == 2){
-            imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[1] as UIImage, frame: outerFrame)])
+            imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[1] as! UIImage, frame: outerFrame)])
             currentViewNumber = currentPhoto
-            (imageViewArray[0] as ZoomablePhotoView).pageNumber = 0
-            (imageViewArray[1] as ZoomablePhotoView).pageNumber = 1
+            (imageViewArray[0] as! ZoomablePhotoView).pageNumber = 0
+            (imageViewArray[1] as! ZoomablePhotoView).pageNumber = 1
         }else{
             if (currentNumber == 0){
-                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[1] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[2] as UIImage, frame: outerFrame)])
+                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[0] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[1] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[2] as! UIImage, frame: outerFrame)])
                 currentViewNumber = 0
                 for i in 0...2{
-                    (imageViewArray[i] as ZoomablePhotoView).pageNumber = i
+                    (imageViewArray[i] as! ZoomablePhotoView).pageNumber = i
                 }
-            }else if (currentNumber == photosArray.count - 1){
-                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[currentPhoto - 2] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto - 1] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto] as UIImage, frame: outerFrame)])
+            }else if (currentNumber >= photosArray.count - 1){
+                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[currentPhoto - 2] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto - 1] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto] as! UIImage, frame: outerFrame)])
                 currentViewNumber = 2
                 for i in 0...2{
-                    (imageViewArray[(currentPhoto - 2 + i) % 3] as ZoomablePhotoView).pageNumber = currentPhoto - 2 + i
+                    (imageViewArray[(currentPhoto - 2 + i) % 3] as! ZoomablePhotoView).pageNumber = currentPhoto - 2 + i
                 }
             }else{
-                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[currentPhoto - 1] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto] as UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto + 1] as UIImage, frame: outerFrame)])
+                imageViewArray = NSArray(array: [ZoomablePhotoView(picture: photosArray[currentPhoto - 1] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto] as! UIImage, frame: outerFrame), ZoomablePhotoView(picture: photosArray[currentPhoto + 1] as! UIImage, frame: outerFrame)])
                 currentViewNumber = 1
                 for i in 0...2{
-                    (imageViewArray[(currentPhoto - 1 + i) % 3] as ZoomablePhotoView).pageNumber = currentPhoto - 1 + i
+                    (imageViewArray[(currentPhoto - 1 + i) % 3] as! ZoomablePhotoView).pageNumber = currentPhoto - 1 + i
                 }
             }
         }
@@ -75,9 +75,9 @@ class PhotoCollevtionView: UIView, UIScrollViewDelegate {
         contentView.delegate = self
         
         for i in 0...(imageViewArray.count - 1){
-            contentView.addSubview(imageViewArray[i] as UIView)
-            (imageViewArray[i] as ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)((imageViewArray[i] as ZoomablePhotoView).pageNumber) * contentView.frame.width + halfPhotoGap, y: 0)
-            (imageViewArray[i] as ZoomablePhotoView).userInteractionEnabled = true
+            contentView.addSubview(imageViewArray[i] as! UIView)
+            (imageViewArray[i] as! ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)((imageViewArray[i] as! ZoomablePhotoView).pageNumber) * contentView.frame.width + halfPhotoGap, y: 0)
+            (imageViewArray[i] as! ZoomablePhotoView).userInteractionEnabled = true
         }
         
         contentView.contentSize = CGSize(width: contentView.frame.width * (CGFloat)(photosArray.count), height: contentView.frame.height)
@@ -100,18 +100,18 @@ class PhotoCollevtionView: UIView, UIScrollViewDelegate {
                 currentViewNumber = (currentViewNumber + 1) % 3
                 currentPhoto++
                 if currentPhoto < photosArray.count - 1{
-                    (imageViewArray[(currentViewNumber + 1) % 3] as ZoomablePhotoView).pageNumber = (currentPhoto + 1)
-                    (imageViewArray[(currentViewNumber + 1) % 3] as ZoomablePhotoView).assignPhoto(picture: photosArray[currentPhoto + 1] as UIImage)
-                    (imageViewArray[(currentViewNumber + 1) % 3] as ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)(currentPhoto + 1) * contentView.frame.width + halfPhotoGap, y: 0)
+                    (imageViewArray[(currentViewNumber + 1) % 3] as! ZoomablePhotoView).pageNumber = (currentPhoto + 1)
+                    (imageViewArray[(currentViewNumber + 1) % 3] as! ZoomablePhotoView).assignPhoto(picture: photosArray[currentPhoto + 1] as! UIImage)
+                    (imageViewArray[(currentViewNumber + 1) % 3] as! ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)(currentPhoto + 1) * contentView.frame.width + halfPhotoGap, y: 0)
                 }
                 
             }else if contentView.contentOffset.x < (imageViewArray[currentViewNumber].frame.origin.x - 0.7 * contentView.frame.width){
                 currentViewNumber = (currentViewNumber + 2) % 3
                 currentPhoto--
                 if currentPhoto > 0{
-                    (imageViewArray[(currentViewNumber + 2) % 3] as ZoomablePhotoView).pageNumber = (currentPhoto - 1)
-                    (imageViewArray[(currentViewNumber + 2) % 3] as ZoomablePhotoView).assignPhoto(picture: photosArray[currentPhoto - 1] as UIImage)
-                    (imageViewArray[(currentViewNumber + 2) % 3] as ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)(currentPhoto - 1) * contentView.frame.width + halfPhotoGap, y: 0)
+                    (imageViewArray[(currentViewNumber + 2) % 3] as! ZoomablePhotoView).pageNumber = (currentPhoto - 1)
+                    (imageViewArray[(currentViewNumber + 2) % 3] as! ZoomablePhotoView).assignPhoto(picture: photosArray[currentPhoto - 1] as! UIImage)
+                    (imageViewArray[(currentViewNumber + 2) % 3] as! ZoomablePhotoView).frame.origin = CGPoint(x: (CGFloat)(currentPhoto - 1) * contentView.frame.width + halfPhotoGap, y: 0)
                 }
                 
             }
@@ -124,7 +124,7 @@ class PhotoCollevtionView: UIView, UIScrollViewDelegate {
         super.layoutSubviews()
         
         for i in 0...(imageViewArray.count - 1){
-            (imageViewArray[i] as ZoomablePhotoView).frame = CGRect(x: (CGFloat)((imageViewArray[i] as ZoomablePhotoView).pageNumber) * contentView.frame.width + halfPhotoGap, y: 0, width: self.frame.width, height: self.frame.height)
+            (imageViewArray[i] as! ZoomablePhotoView).frame = CGRect(x: (CGFloat)((imageViewArray[i] as! ZoomablePhotoView).pageNumber) * contentView.frame.width + halfPhotoGap, y: 0, width: self.frame.width, height: self.frame.height)
         }
         contentView.contentSize = CGSize(width: contentView.frame.width * (CGFloat)(photosArray.count), height: contentView.frame.height)
         contentView.contentOffset = CGPoint(x: (CGFloat)(currentPhoto) * contentView.frame.width, y: 0)
--- PhotoCollectionView/ViewController.swift
+++ PhotoCollectionView/ViewController.swift
@@ -12,13 +12,14 @@ class ViewController: UIViewController {
 
     var view1 : PhotoCollevtionView?
     
-    override init(){
+    init(){
         super.init(nibName: nil, bundle: nil)
         
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.frame = CGRect(x: 10, y: 10, width: 100, height: 100)
         var image1 = UIImage(named: "wide")
         var image2 = UIImage(named: "square")
         var image3 = UIImage(named: "high")
@@ -26,7 +27,7 @@ class ViewController: UIViewController {
         var image5 = UIImage(named: "pic5")
         var image6 = UIImage(named: "pic6")
         var image7 = UIImage(named: "pic7")
-        var array = NSArray(array: [image1!,image2!])
+        var array = NSArray(array: [image1!, image2!, image3!, image4!, image5!])
         view1 = PhotoCollevtionView(outerFrame: self.view.frame, photoArray: array)
         self.view.addSubview(view1!)
         // Do any additional setup after loading the view, typically from a nib.
